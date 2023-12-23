84cab12be4722aec848c5fc68bafc59de1772285
oINCLUDEo <nikita-zhuklin@rambler.ru>
2023-12-23 21:02:45 +0400

    add server and client

--- client.py
+++ client.py
@@ -9,7 +9,9 @@ def receive_messages():
         try:
             message = client_socket.recv(1024).decode('utf-8')
             if message:
+                chat_text.config(state='normal')
                 chat_text.insert(tk.END, message + '\n')
+                chat_text.config(state='disabled')
         except:
             break
 
@@ -21,10 +23,7 @@ def send_message():
         message_entry.delete(0, tk.END)
 
 
-def login():
-    username = username_entry.get()
-    password = password_entry.get()
-
+def login(username, password):
     client_socket.send(username.encode('utf-8'))
     client_socket.send(password.encode('utf-8'))
 
@@ -38,14 +37,15 @@ def login():
 
 def open_chat(username):
     chat_window = tk.Tk()
-    chat_window.title(f"Chat - {username}")
+    chat_window.title(f"Открыт чат для пользователя - {username}")
 
-    chat_label = tk.Label(chat_window, text=f"Welcome, {username}!", font=("Arial", 12))
+    chat_label = tk.Label(chat_window, text=f"Добро пожаловать, {username}!", font=("Segoe UI", 14), bg='#3300cc')
     chat_label.pack(pady=10)
 
     global chat_text
     chat_text = tk.Text(chat_window, height=20, width=50)
     chat_text.pack()
+    chat_text.config(state='disabled')
 
     global message_entry
     message_entry = tk.Entry(chat_window, width=40)
@@ -62,22 +62,57 @@ def open_chat(username):
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('localhost', 9999))
 
-login_window = tk.Tk()
-login_window.title("Login")
 
-username_label = tk.Label(login_window, text="Username")
-username_label.pack()
+def log_window():
+    def focus_in(e=None):
+        login_button.configure(fg='#3300cc')  # Задаём кнопке нужные цвета
+        login_button.configure(bg='#fff')
 
-username_entry = tk.Entry(login_window)
-username_entry.pack()
+    def focus_out(e=None):
+        login_button.configure(bg='#3300cc')
+        login_button.configure(fg='#fff')
 
-password_label = tk.Label(login_window, text="Password")
-password_label.pack()
+    global login_window
+    login_window = tk.Tk()
+    login_window.geometry('1000x750')  # Задаём размер
+    login_window.title("Messenger")
+    login_window.resizable(width=False, height=False)
 
-password_entry = tk.Entry(login_window, show="*")
-password_entry.pack()
+    canvas = tk.Canvas(login_window, width=1000, height=750)
+    canvas.pack()
+    canvas.pack_propagate(False)
+    img = tk.PhotoImage(file='back.gif')
+    canvas.create_image(0, 0, anchor=tk.NW, image=img)
 
-login_button = tk.Button(login_window, text="Login", command=login)
-login_button.pack(pady=10)
+    auth_canvas = tk.Canvas(canvas, width=250, height=300, bg='white')
+    auth_canvas.place(x='375', y='225')
+    auth_canvas.pack_propagate(False)
 
-login_window.mainloop()
+    username_text = auth_canvas.create_text(125, 20, text="Имя пользователя", fill="Black", font=('Perpetua', 14))
+    username_entry = tk.Entry(auth_canvas, width=20, font=('Segoe UI', 14))
+    username_entry.config(highlightbackground="black")
+
+    password_text = auth_canvas.create_text(125, 95, text="Пароль", fill="Black", font=('Perpetua', 14))
+    password_entry = tk.Entry(auth_canvas, width=20, show="*", font=('Segoe UI', 14))
+
+    login_button = tk.Button(auth_canvas, text="Вход в аккаунт",
+                             command=lambda: login(username_entry.get(), password_entry.get()), pady='5',
+                             cursor='hand2', fg='#fff', bg='#3300cc',
+                             font=('Perpetua', 14))
+    login_button.bind('<Enter>', focus_in)  # При входе курсора в область кнопки выполняем focus_in
+    login_button.bind('<Leave>', focus_out)  # При выходе курсора из области кнопки выполняем focus_out_out
+    register_button = tk.Label(auth_canvas, text="Регистрация", fg='blue', bg='white', cursor='hand2',
+                               font=('Perpetua', 14, 'underline'))
+
+    username_entry.pack_propagate(False)
+    username_entry.place(x='35', y='40')
+
+    password_entry.place(x='35', y='120')
+
+    login_button.place(x='50', y='160')
+    register_button.place(x='65', y='220')
+
+    login_window.mainloop()
+
+
+log_window()
--- server.py
+++ server.py
@@ -1,12 +1,18 @@
 import socket
 import threading
+import sqlite3
 
-# Dictionary to store user credentials (replace this with a proper authentication system)
-user_credentials = {
-    "user1": "password1",
-    "user2": "password2",
-    "user3": "password3"
-}
+
+# Function to check user credentials from the SQLite database
+def check_credentials(username, password):
+    conn = sqlite3.connect('user_credentials.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+    user = cursor.fetchone()
+
+    conn.close()
+    return user is not None
 
 
 # Function to handle client connection
@@ -39,16 +45,13 @@ while True:
     client_socket, addr = server.accept()
     username = client_socket.recv(1024).decode('utf-8')
 
-    if username in user_credentials:
-        password = client_socket.recv(1024).decode('utf-8')
-        if password == user_credentials[username]:
-            clients.append(client_socket)
-            client_socket.send("Вход выполнен успешно".encode('utf-8'))
-            print(f"{username} connected")
-            threading.Thread(target=handle_client, args=(client_socket, username)).start()
-        else:
-            client_socket.send("Invalid credentials".encode('utf-8'))
-            client_socket.close()
+    password = client_socket.recv(1024).decode('utf-8')
+
+    if not check_credentials(username, password):
+        clients.append(client_socket)
+        client_socket.send("Вход выполнен успешно".encode('utf-8'))
+        print(f"{username} connected")
+        threading.Thread(target=handle_client, args=(client_socket, username)).start()
     else:
-        client_socket.send("User not found".encode('utf-8'))
+        client_socket.send("Invalid credentials".encode('utf-8'))
         client_socket.close()
