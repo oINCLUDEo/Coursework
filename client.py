b1a21fdfa6fca2ddd4a9f4321210ea3cf6666434
oINCLUDEo <nikita-zhuklin@rambler.ru>
2023-12-24 18:42:01 +0400

    added loading history added registration new interface fixed bugs

--- client.py
+++ client.py
@@ -1,10 +1,11 @@
+import json
 import tkinter as tk
 from tkinter import messagebox
 import socket
 import threading
 
 
-def receive_messages():
+def handle_messages():
     while True:
         try:
             message = client_socket.recv(1024).decode('utf-8')
@@ -12,25 +13,41 @@ def receive_messages():
                 chat_text.config(state='normal')
                 chat_text.insert(tk.END, message + '\n')
                 chat_text.config(state='disabled')
+                chat_text.see(tk.END)
         except:
             break
 
 
-def send_message():
-    message = message_entry.get()
-    if message:
-        client_socket.send(message.encode('utf-8'))
-        message_entry.delete(0, tk.END)
-
-
 def login(username, password):
+    option = "login"
+    client_socket.send(option.encode('utf-8'))
+
     client_socket.send(username.encode('utf-8'))
     client_socket.send(password.encode('utf-8'))
 
+    print('работаем')
     response = client_socket.recv(1024).decode('utf-8')
-    if response == "Вход выполнен успешно":
+    print('не работаем')
+    if response == "Login successful":
         login_window.destroy()
         open_chat(username)
+    else:
+        messagebox.showerror("Error", response)
+
+
+def register(username, password):
+    option = "register"
+    client_socket.send(option.encode('utf-8'))
+
+    client_socket.send(username.encode('utf-8'))
+    client_socket.send(password.encode('utf-8'))
+    print('пытаюсь')
+    response = client_socket.recv(1024).decode('utf-8')
+    if response == "Registration successful":
+        messagebox.showinfo("Success", "Registration successful. You can now log in.")
+        reg_canvas.destroy()
+        # canvas.delete(register_text)
+        # log_window(False)
     else:
         messagebox.showerror("Error", response)
 
@@ -51,19 +68,87 @@ def open_chat(username):
     message_entry = tk.Entry(chat_window, width=40)
     message_entry.pack(pady=10)
 
+    def send_message():
+        message = message_entry.get()
+        client_socket.send(message.encode('utf-8'))
+        message_entry.delete(0, tk.END)
+
     send_button = tk.Button(chat_window, text="Send", command=send_message)
     send_button.pack()
 
-    threading.Thread(target=receive_messages).start()
+    threading.Thread(target=handle_messages).start()
 
     chat_window.mainloop()
 
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('localhost', 9999))
+def reg_window_username(event):
+    auth_canvas.destroy()
+    global register_text
+    register_text = canvas.create_text(500, 100, anchor='center', text='        СОЗДАНИЕ\n УЧЕТНОЙ ЗАПИСИ',
+                                       fill="White",
+                                       font=('Perpetua', 32, 'bold'))
+
+    def focus_in(e=None):
+        next_button.configure(fg='#3300cc')  # Задаём кнопке нужные цвета
+        next_button.configure(bg='#fff')
+
+    def focus_out(e=None):
+        next_button.configure(bg='#3300cc')
+        next_button.configure(fg='#fff')
+
+    def check_data():
+        print('ку')
+        if len(username_entry.get().rstrip(' ')) >= 1:
+            username = username_entry.get()
+            reg_window_password(username)
+
+    global reg_canvas
+    reg_canvas = tk.Canvas(canvas, width=250, height=300, bg='white')
+    reg_canvas.place(x='375', y='225')
+    reg_canvas.pack_propagate(False)
+
+    global username_text, username_entry, next_button
+    username_text = reg_canvas.create_text(125, 70, text="Имя пользователя", fill="Black", font=('Perpetua', 14))
+    username_entry = tk.Entry(reg_canvas, width=20, font=('Segoe UI', 14))
+    username_entry.config(highlightbackground="black")
+
+    next_button = tk.Button(reg_canvas, text="Далее", pady='5',
+                            cursor='hand2', command=check_data, fg='#fff', bg='#3300cc',
+                            font=('Perpetua', 14))
+    next_button.bind('<Enter>', focus_in)  # При входе курсора в область кнопки выполняем focus_in
+    next_button.bind('<Leave>', focus_out)  # При выходе курсора из области кнопки выполняем focus_out_out
+    # register_button = tk.Label(auth_canvas, text="Регистрация", fg='blue', bg='white', cursor='hand2',
+    #                           font=('Perpetua', 14, 'underline'))
+    # register_button.bind("<Button-1>", reg_window)
+
+    username_entry.pack_propagate(False)
+    username_entry.place(x='25', y='100')
+
+    next_button.place(x='95', y='160')
+    # register_button.place(x='65', y='220')
 
 
-def log_window():
+def reg_window_password(username):
+    def check_data():
+        if len(password_entry.get().rstrip(' ')) >= 1:
+            print('Проверил данные')
+            register(username, password_entry.get())
+
+    reg_canvas.delete(username_text)
+    username_entry.destroy()
+    next_button.destroy()
+    password_text = reg_canvas.create_text(125, 70, text="Пароль", fill="Black", font=('Perpetua', 14))
+    password_entry = tk.Entry(reg_canvas, width=20, show="*", font=('Segoe UI', 14))
+
+    next_button2 = tk.Button(reg_canvas, text="Далее", pady='5',
+                             cursor='hand2', command=check_data, fg='#fff', bg='#3300cc',
+                             font=('Perpetua', 14))
+
+    next_button2.place(x='95', y='160')
+    password_entry.place(x='25', y='100')
+
+
+def log_window(k):
     def focus_in(e=None):
         login_button.configure(fg='#3300cc')  # Задаём кнопке нужные цвета
         login_button.configure(bg='#fff')
@@ -72,22 +157,24 @@ def log_window():
         login_button.configure(bg='#3300cc')
         login_button.configure(fg='#fff')
 
-    global login_window
+    global client_socket, login_window
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', 9090))
     login_window = tk.Tk()
     login_window.geometry('1000x750')  # Задаём размер
     login_window.title("Messenger")
     login_window.resizable(width=False, height=False)
-
+    global canvas
     canvas = tk.Canvas(login_window, width=1000, height=750)
     canvas.pack()
     canvas.pack_propagate(False)
     img = tk.PhotoImage(file='back.gif')
     canvas.create_image(0, 0, anchor=tk.NW, image=img)
-
+    global auth_canvas
     auth_canvas = tk.Canvas(canvas, width=250, height=300, bg='white')
     auth_canvas.place(x='375', y='225')
     auth_canvas.pack_propagate(False)
-
+    print('норм')
     username_text = auth_canvas.create_text(125, 20, text="Имя пользователя", fill="Black", font=('Perpetua', 14))
     username_entry = tk.Entry(auth_canvas, width=20, font=('Segoe UI', 14))
     username_entry.config(highlightbackground="black")
@@ -95,14 +182,15 @@ def log_window():
     password_text = auth_canvas.create_text(125, 95, text="Пароль", fill="Black", font=('Perpetua', 14))
     password_entry = tk.Entry(auth_canvas, width=20, show="*", font=('Segoe UI', 14))
 
-    login_button = tk.Button(auth_canvas, text="Вход в аккаунт",
-                             command=lambda: login(username_entry.get(), password_entry.get()), pady='5',
+    login_button = tk.Button(auth_canvas, text="Вход в аккаунт", pady='5',
+                             command=lambda: login(username_entry.get(), password_entry.get()),
                              cursor='hand2', fg='#fff', bg='#3300cc',
                              font=('Perpetua', 14))
     login_button.bind('<Enter>', focus_in)  # При входе курсора в область кнопки выполняем focus_in
     login_button.bind('<Leave>', focus_out)  # При выходе курсора из области кнопки выполняем focus_out_out
     register_button = tk.Label(auth_canvas, text="Регистрация", fg='blue', bg='white', cursor='hand2',
                                font=('Perpetua', 14, 'underline'))
+    register_button.bind("<Button-1>", reg_window_username)
 
     username_entry.pack_propagate(False)
     username_entry.place(x='35', y='40')
@@ -111,8 +199,8 @@ def log_window():
 
     login_button.place(x='50', y='160')
     register_button.place(x='65', y='220')
+    if k:
+        login_window.mainloop()
 
-    login_window.mainloop()
 
-
-log_window()
+log_window(True)
--- server.py
+++ server.py
@@ -3,7 +3,32 @@ import threading
 import sqlite3
 
 
-# Function to check user credentials from the SQLite database
+# Functions for database and user handling (create_database, register_user, check_credentials) remain unchanged
+def register_user(username, password):
+    conn = sqlite3.connect('user_credentials.db')
+    cursor = conn.cursor()
+
+    try:
+        print('скул передает привет')
+        info = cursor.execute('SELECT * FROM users WHERE username=?', (username,))
+        if info.fetchone() is None:
+            # Делаем когда нету человека в бд
+            cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+            print('скул работает')
+            conn.commit()
+            conn.close()
+            return True
+        else:
+            # Делаем когда есть человек в бд
+            print('скул передает привет2')
+            conn.close()
+            return False
+    except sqlite3.IntegrityError:
+        print('скул нашел ошибку')
+        conn.close()
+        return False
+
+
 def check_credentials(username, password):
     conn = sqlite3.connect('user_credentials.db')
     cursor = conn.cursor()
@@ -15,43 +40,111 @@ def check_credentials(username, password):
     return user is not None
 
 
-# Function to handle client connection
+def create_chat_history_table():
+    conn = sqlite3.connect('chat_history.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS messages (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    username TEXT,
+                    message TEXT
+                )''')
+    conn.commit()
+    conn.close()
+
+
+# Function to save messages to the database
+def save_message_to_db(username, message):
+    conn = sqlite3.connect('chat_history.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO messages (username, message) VALUES (?, ?)", (username, message))
+    conn.commit()
+    conn.close()
+
+
+# Function to retrieve chat history from the database
+def retrieve_chat_history():
+    conn = sqlite3.connect('chat_history.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM messages")
+    history = c.fetchall()
+    conn.close()
+    return history
+
+
 def handle_client(client_socket, username):
+    global chat_history
+
+    # Send chat history to the client upon connection
+    for message in chat_history:
+        client_socket.send(f"\n{message[1]}: {message[2]}".encode('utf-8'))
+    print('прилет')
     while True:
         try:
             message = client_socket.recv(1024).decode('utf-8')
             if message:
+                save_message_to_db(username, message)
+                chat_history.append((len(chat_history) + 1, username, message))
                 broadcast(f"{username}: {message}")
-        except:
+        except Exception as e:
+            print(f"Error: {e}")
             break
 
 
-# Function to broadcast messages to all clients
 def broadcast(message):
     for client in clients:
-        client.send(message.encode('utf-8'))
+        try:
+            client.send(message.encode('utf-8'))
+        except Exception as e:
+            print(f"Error sending message: {e}")
+            clients.remove(client)
+            client.close()
 
 
-# Start the server
+# Server setup
+create_chat_history_table()
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('localhost', 9999))
-server.listen()
+server.bind(('localhost', 9090))
+server.listen(100)
 
+# List to store connected clients
 clients = []
 
-print("Server is running...")
+# Load chat history from the database
+chat_history = retrieve_chat_history()
 
-while True:
-    client_socket, addr = server.accept()
-    username = client_socket.recv(1024).decode('utf-8')
 
-    password = client_socket.recv(1024).decode('utf-8')
+# Accept and handle client connections
+def accept_clients():
+    while True:
+        client_socket, addr = server.accept()
+        option = client_socket.recv(1024).decode('utf-8')
 
-    if not check_credentials(username, password):
-        clients.append(client_socket)
-        client_socket.send("Вход выполнен успешно".encode('utf-8'))
-        print(f"{username} connected")
-        threading.Thread(target=handle_client, args=(client_socket, username)).start()
-    else:
-        client_socket.send("Invalid credentials".encode('utf-8'))
-        client_socket.close()
+        if option.lower() == "login":
+            username = client_socket.recv(1024).decode('utf-8')
+            password = client_socket.recv(1024).decode('utf-8')
+
+            if check_credentials(username, password):
+                clients.append(client_socket)
+                client_socket.send("Login successful".encode('utf-8'))
+                print(f"{username} connected")
+
+                threading.Thread(target=handle_client, args=(client_socket, username)).start()
+            else:
+                client_socket.send("Invalid credentials".encode('utf-8'))
+                client_socket.close()
+        elif option.lower() == "register":
+            username = client_socket.recv(1024).decode('utf-8')
+            password = client_socket.recv(1024).decode('utf-8')
+
+            if register_user(username, password):
+                client_socket.send("Registration successful".encode('utf-8'))
+            else:
+                client_socket.send("Username already exists".encode('utf-8'))
+                client_socket.close()
+
+
+# Rest of the server code remains the same
+
+# Create a separate thread to accept incoming connections
+threading.Thread(target=accept_clients).start()
